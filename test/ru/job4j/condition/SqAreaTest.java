0642ba0df5f3e842ea8dc613e0f71a5704050578
EkaterinaSKuznetsova <ek.belogurova@gmail.com>
2021-07-14 12:30:12 +0300

    3. Тесты для определения между точками. [# 159459]

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class SqAreaTest {
+
+   @Test
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP8K1Square4() {
+        int expected = 4;
+        int p = 8;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP5K1Square1and56() {
+        double expected = 1.56;
+        int p = 5;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP12K4Square5and76() {
+        double expected = 5.76;
+        int p = 12;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
