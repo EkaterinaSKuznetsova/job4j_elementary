f11bf585e10f806cf8a7484e0ceb0e91bc75324b
ek.belogurova@gmail.com <2511balablaXam2511>
2021-07-23 12:15:55 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ekaterina Kuznetsova";
+        names[1] = "Svetlana Zueva";
+        names[2] = "Vladimir Marusenko";
+        names[3] = "Alexey Gorshkov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
