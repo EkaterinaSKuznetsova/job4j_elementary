71dadbadc1d2b67bec2b70050e0b72e3aa4b15ac
ek.belogurova@gmail.com <2511balablaXam2511>
2021-08-18 21:05:15 +0300

    3. Перегрузить метод max для трех чисел [# 122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,12 +1,17 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
+    public int max(int left, int right) {
         return left >= right ? left : right;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Max.max(8, 9));
+    public int max(int first, int second, int third) {
+        return max(first, max(second, third));
     }
+
+    public int max(int first, int second, int third, int fourth) {
+        return max(first, max(second, third, fourth));
+    }
+
 
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,28 +8,41 @@ public class MaxTest {
 
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        Max maxTwo = new Max();
+        int result = maxTwo.max(1, 2);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax5To2Then5() {
-        int left = 5;
-        int right = 2;
-        int result = Max.max(left, right);
+        Max maxTwo = new Max();
+        int result = maxTwo.max(5, 2);
         int expected = 5;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
+        Max maxTwo = new Max();
+        int result = maxTwo.max(2, 2);
         int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMaxFromThirdNumbers() {
+        Max maxTwo = new Max();
+        int result = maxTwo.max(2, 3, 6);
+        int expected = 6;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMaxFromFourthNumbers() {
+        Max maxTwo = new Max();
+        int result = maxTwo.max(9, 2, 0, 1);
+        int expected = 9;
         Assert.assertEquals(result, expected);
     }
 }
